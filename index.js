eb1217b2e490918e8809497ad6dd59795a23be4f
SpiColorPendra <zongmingli@outlook.com>
2016-03-19 20:38:22 +0800

    add routes and update index.js

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ var express = require('express');
 var app = express();
 
 app.listen(3000, () => {
-  console.log('server start');
+    console.log('server start');
+});
+
+app.get('/', function (req, res) {
+  res.end('Hello Node');
 });
