b1e7e231bd78c0c9dc2c0b9a2297ff50208b6c64
SpiColorPendra <zongmingli@outlook.com>
2016-03-20 15:15:22 +0800

    重命名index.js为app.js 修改了package.json添加了依赖信息 app.js里面配置了环境信息，静态文件路径，路由文件路径，404，500错误追踪

---
+++ app.js
@@ -0,0 +1,78 @@
+/**
+ * Module dependencies
+ */
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// Load Routes
+var timer = require('./routes/timer');
+var users = require('./routes/users');
+
+// create app instance
+var app = express();
+
+// set static views path and view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// set log on dev
+app.use(logger('dev'));
+app.use(bodyParser.json());
+// cookies parser
+app.use(cookieParser());
+// static files dic express托管静态文件目录
+app.use(express.static(path.join(__dirname, 'public')));
+
+// 配置路由
+app.use('/', routes);
+app.use('/users/', users);
+
+// 404错误统一处理
+app.use((req, res, next) => {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+// 开发环境，500错误接收，输出堆栈追踪信息
+if (app.get('env') == 'development') {
+    app.use((err, req, res, next) => {
+        res, status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// 生产环境，500错出处理
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
+/*
+ 这个文件不再处理路由信息，处理http请求的事情交给routes/目录下
+ 对应的路由处理
+app.listen(3000, () => {
+    console.log('server start');
+});
+
+app.get('/', function (req, res) {
+    res.end('Hello Node');
+});
+
+app.get('/user', (req, res) => {
+    console.log('hello user');
+});
+
+app.get('/user/:id', (req, res) => {
+    console.log('hello id');
+});
+*/
