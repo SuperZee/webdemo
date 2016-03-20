e38e9de9e2cfc1f8fd7394148b49ea7e509bbbe1
SpiColorPendra <zongmingli@outlook.com>
2016-03-20 15:48:51 +0800

    注释了timer.js里面的调试信息,修复了www console颜色穿越的问题

--- routes/timer.js
+++ routes/timer.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // POST
 router.get('/timer', (req, res, next) => {
-    console.log('timer');
+    //console.log('timer');
     res.render('index', {title: ' TIMER', name: 'SuperZee'});
 });
 
